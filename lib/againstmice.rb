ff82b226b89b857f996e806085ea466a78e99e9f
christian <rewindforward@gmail.com>
2021-05-21 06:02:01 -0400

    Cleans code, makes data dir

--- lib/againstmice.rb
+++ lib/againstmice.rb
@@ -1,66 +1,66 @@
 require_relative('cat')
 
 class Againstmice
-    attr_accessor :tally, :cat1, :cat2, :mice
-    
-    def initialize(cat1, cat2)
-        @cat1, @cat2 = cat1, cat2
-        @tally = 0
-    end
-    
-    def self.spawn
-        @mice = []
-        count = 100
-        while count > 0 do
-            agility = rand(1..100)
-            if agility < 100 && agility > 85
-                @mice << 3
-            elsif agility < 85 && agility > 60
-                @mice << 2
-            else
-                @mice << 1
-            end
-            @mice
-            count -= 1
-        end
-        @mice
-    end
-    
-    def self.compete(*tally)
-        @tally = 0
-        mice1 = spawn
-        mice2 = spawn
-        mice1.zip(mice2).map do |x, y|
-            if x > y    
-                puts "Cat caught mouse!" 
-                @tally += 1
-            else
-                puts "Mouse ran away!"
-            end
-        end
-        return @tally
-    end
-    
-    def self.results
-        puts cat_1_chance_to_win = compete(@cat1)
-        puts cat_2_chance_to_win = compete(@cat2)
-        
-        if cat_1_chance_to_win > cat_2_chance_to_win
-            puts "Cat 1 wins, Cat 2 looses"
-        else
-            puts "Cat 2 wins, Cat 1 looses"
-        end
-    end
+	attr_accessor :tally, :cat1, :cat2, :mice
 
-    def self.run
-        puts "against mice!"
-        puts "enter player one's cat name"
-        cat_one = gets.chomp
-        puts "enter player two's cat name"
-        cat_two = gets.chomp
-        against_mice = Againstmice.new(Cat.new(cat_one), Cat.new(cat_two))
-        spawn
-        compete
-        results
-    end
+	def initialize(cat1, cat2)
+		@cat1, @cat2 = cat1, cat2
+		@tally = 0
+	end
+
+	def self.spawn
+		@mice = []
+		count = 100
+		while count > 0 do
+			agility = rand(1..100)
+			if agility < 100 && agility > 85
+				@mice << 3
+			elsif agility < 85 && agility > 60
+				@mice << 2
+			else
+				@mice << 1
+			end
+			@mice
+			count -= 1
+		end
+		@mice
+	end
+
+	def self.compete(*tally)
+		@tally = 0
+		mice1 = spawn
+		mice2 = spawn
+		mice1.zip(mice2).map do |x, y|
+			if x > y
+				puts "Cat caught mouse!"
+				@tally += 1
+			else
+				puts "Mouse ran away!"
+			end
+		end
+		return @tally
+	end
+
+	def self.results
+		puts cat_1_chance_to_win = compete(@cat1)
+		puts cat_2_chance_to_win = compete(@cat2)
+
+		if cat_1_chance_to_win > cat_2_chance_to_win
+			puts "Cat 1 wins, Cat 2 looses"
+		else
+			puts "Cat 2 wins, Cat 1 looses"
+		end
+	end
+
+	def self.run
+		puts "against mice!"
+		puts "enter player one's cat name"
+		cat_one = gets.chomp
+		puts "enter player two's cat name"
+		cat_two = gets.chomp
+		against_mice = Againstmice.new(Cat.new(cat_one), Cat.new(cat_two))
+		spawn
+		compete
+		results
+	end
 end
--- lib/player.rb
+++ lib/player.rb
@@ -1,54 +1,54 @@
 require_relative('cat.rb')
 
 class Player
-    attr_accessor :name, :cat, :pawz
-    
-    def initialize(name)
-        @name = name
-        @cat = Cat.new("Kitty")
-        @pawz = 400
-    end
+	attr_accessor :name, :cat, :pawz
+	
+	def initialize(name)
+		@name = name
+		@cat = Cat.new("Kitty")
+		@pawz = 400
+	end
 
-    def self.create
-        player_array = []
-        puts "create a player!\n"
-        puts "would you like to create a new player? (y/n)"
-        user_input = gets.chomp
-        if user_input == 'y'
-            puts "what would you like to name your player? "
-            player_name = gets.chomp
-            player = Player.new(player_name)
-            player_array=[] << player
-        elsif user_input == 'n'
-        player_array
-        end
-    end
+	def self.create
+		player_array = []
+		puts "create a player!\n"
+		puts "would you like to create a new player? (y/n)"
+		user_input = gets.chomp
+		if user_input == 'y'
+			puts "what would you like to name your player? "
+			player_name = gets.chomp
+			player = Player.new(player_name)
+			player_array=[] << player
+		elsif user_input == 'n'
+		player_array
+		end
+	end
 
-    def self.all
-        players = []
-        File.open('./saves/players', 'r') do |file|
-          file.each do |line|
-            name = line
-            players << Player.new(name.chomp!)
-          end
-        end
-        players.each do |player|
-            puts player.name
-        end
-        players
-    end
+	def self.all
+		players = []
+		File.open('./saves/players', 'r') do |file|
+		  file.each do |line|
+			name = line
+			players << Player.new(name.chomp!)
+		  end
+		end
+		players.each do |player|
+			puts player.name
+		end
+		players
+	end
 
-    def self.find_by_name(name)
-        players = []
-        all.each do |user|
-            if  name == user
-                puts "Match! Found user by name.\n"
-                puts "Query: #{name}. Player name"
-                players << Player.new(user.name)
-            else
-                puts "No!"
-            end
-        end
-        players
-    end
+	def self.find_by_name(name)
+		players = []
+		all.each do |user|
+			if  name == user
+				puts "Match! Found user by name.\n"
+				puts "Query: #{name}. Player name"
+				players << Player.new(user.name)
+			else
+				puts "No!"
+			end
+		end
+		players
+	end
 end
